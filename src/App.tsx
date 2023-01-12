b117a508d4c6fc50cc661d7416df9342d1ddc384
Onoja Victor <onoja.jsdev@gmail.com>
2023-01-12 16:19:37 +0100

    setup login file

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react'; 
 import './App.css';
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import Login from './components/pages/Login';
 
 function App() {
   return (
-    <div className="App"> 
-    <h1>HELLO TYPESCRIPT</h1>
-    </div>
+   <BrowserRouter>
+   <Routes>
+    <Route element={<Login/>} path="/" />
+   </Routes>
+   </BrowserRouter>
   );
 }
 
---
+++ src/components/pages/Login.tsx
@@ -0,0 +1,15 @@
+import "../styles/login.css"
+
+export default function Login()  {
+
+
+
+    return(
+        <div className="login_container">
+            Login Here
+            <p>what is your name</p>
+        </div>
+    )
+}
+
+ 
