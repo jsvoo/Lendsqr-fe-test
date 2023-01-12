1389a8617229a8f3038b630867cd9b2de47740c1
Onoja Victor <onoja.jsdev@gmail.com>
2023-01-12 22:53:21 +0100

    added navigation.scss file, imported variables into navigation.scss

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,7 +1,9 @@
+import "./styles/navigation.css"
+
 export default function Navigation(){
     return(
         <div className="navigation-container">
-            THis is the navigation
+            
         </div>
     )
 }
