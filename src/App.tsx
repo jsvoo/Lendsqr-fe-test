7d331f601b16495d0e36f0f4eb305912876d9cf5
Onoja Victor <onoja.jsdev@gmail.com>
2023-01-13 00:32:01 +0100

    added sidebar component

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,15 @@ import './App.css';
 import {BrowserRouter, Routes, Route} from "react-router-dom"
 import Login from './components/pages/Login';
 import Navigation from './components/Navigation';
+import Sidebar from './components/Sidebar';
 
 function App() {
   return (
    <BrowserRouter>
+   <Navigation/>
    <Routes>
     <Route element={<Login/>} path="/" />
-    <Route element={< Navigation />} path="nav" />
+    <Route element={< Sidebar />} path="sidebar" />
    </Routes>
    </BrowserRouter>
   );
---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,9 @@
+import "./styles/sidebar.css"
+export default function Sidebar(){
+
+    return(
+        <div className="sidebar-container"> 
+        
+        </div>
+    )
+}
