d60656ca4c92b4db5d387612643f1bd063dbaf45
Onoja Victor <onoja.jsdev@gmail.com>
2023-01-12 22:46:41 +0100

    added route to navigation component

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import {BrowserRouter, Routes, Route} from "react-router-dom"
 import Login from './components/pages/Login';
+import Navigation from './components/Navigation';
 
 function App() {
   return (
    <BrowserRouter>
    <Routes>
     <Route element={<Login/>} path="/" />
+    <Route element={< Navigation />} path="nav" />
    </Routes>
    </BrowserRouter>
   );
