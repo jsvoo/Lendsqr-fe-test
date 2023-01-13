9aa68d465f108ae16cee35188669a9260e7d3a92
Onoja Victor <onoja.jsdev@gmail.com>
2023-01-13 01:04:38 +0100

    added custom font family Work Sans

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,9 +1,28 @@
 import "./styles/sidebar.css"
+import users from "./icons/users.png"
+import guarantors from "./icons/guarantors.png"
+import loans from "./icons/loans.png"
+import decision from "./icons/decision.png"
+import savings from "./icons/savings.png"
+import loan_request from "./icons/loan-request.png"
+import whitelist from "./icons/whitelist.png"
+import karma from "./icons/karma.png"
+import briefcase from "./icons/briefcase.png" 
+import savings_products from "./icons/savings-products.png"
+import fees_and_pricing from "./icons/fees-and-pricing.png"
+import fees_and_charges from "./icons/fees.png"
+import transactions from "./icons/transactions.png"
+import services from "./icons/services.png"
+import service_account from "./icons/service-account.png"
+import settlements from "./icons/settlements.png"
+import reports from "./icons/reports.png"
 export default function Sidebar(){
-
+    
     return(
         <div className="sidebar-container"> 
-        
+Switch Organization
+BUSINESS
+
         </div>
     )
 }
