7895d07bb157de6ca09321dc540f9d39d6a37710
Onoja Victor <onoja.jsdev@gmail.com>
2023-01-13 12:53:14 +0100

    created route for cards

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,16 @@ import {BrowserRouter, Routes, Route} from "react-router-dom"
 import Login from './components/pages/Login';
 import Navigation from './components/Navigation';
 import Sidebar from './components/Sidebar';
+import Cards from './components/props/Cards';
 
 function App() {
   return (
    <BrowserRouter>
    <Navigation/>
+   <Sidebar/>
    <Routes>
     <Route element={<Login/>} path="/" />
-    <Route element={< Sidebar />} path="sidebar" />
+    <Route element={< Cards />} path="cards" />
    </Routes>
    </BrowserRouter>
   );
