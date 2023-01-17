a25059d990ef37b8aa58aeecfbd5535f1b11813f
Onoja Victor <onoja.jsdev@gmail.com>
2023-01-17 13:54:59 +0100

    moved logo into banner section, modified height

--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -7,43 +7,42 @@ export default function Login() {
 
     return (
         <div className="login_container">
-            <div className="logo-section">
-                <img src={logo} alt="Lendsqr image" />
 
-            </div>
 
             <div className="body">
-                <div className="banner-section"> 
+                <div className="banner-section">
+                    <div className="logo-section">
+                        <img src={logo} alt="Lendsqr image" />
 
-                        <div className="banner">
-                            <img src={banner} alt="welcome banner" />
-
-                        </div>
                     </div>
- 
+                    <div className="banner">
+                        <img src={banner} alt="welcome banner" /> 
+                    </div>
+                </div>
+
 
                 <div className="form-section">
 
-                <div className="form-width">
-             <div className="header">
-             <h1 className="welcome">Welcome!</h1>
-                    <p>Enter details to login</p>
-             </div>
+                    <div className="form-width">
+                        <div className="header">
+                            <h1 className="welcome">Welcome!</h1>
+                            <p>Enter details to login</p>
+                        </div>
 
-                    <form >
-                        <div className="email-section input-container">
-                            <input type="email" placeholder="Email" />
-                        </div>
-                        <div className="password-section input-container">
-                            <input type="password" placeholder="Password" />
-                            <span className="show">SHOW</span>
-                        </div>
-                        <p className="forgot-password">
-                            FORGOT PASSWORD
-                        </p>
-                        <button>LOG IN</button>
-                    </form>
-                </div>
+                        <form >
+                            <div className="email-section input-container">
+                                <input type="email" placeholder="Email" />
+                            </div>
+                            <div className="password-section input-container">
+                                <input type="password" placeholder="Password" />
+                                <span className="show">SHOW</span>
+                            </div>
+                            <p className="forgot-password">
+                                FORGOT PASSWORD
+                            </p>
+                            <button type="button">LOG IN</button>
+                        </form>
+                    </div>
 
                 </div>
             </div>
